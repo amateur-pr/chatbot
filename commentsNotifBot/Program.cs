90e65946666054c960aa64455a6216df5107ff1d
amateur-pr <114609496+amateur-pr@users.noreply.github.com>
2022-09-28 21:11:39 +0200

    Add files via upload

---
+++ commentsNotifBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using System.Threading;using Telegram.Bot.Examples.Polling;
+using Telegram.Bot.Extensions.Polling;
+
+
+
+namespace commentsNotifBot
+{
+    class Program
+    {
+        private static string token { get; set; } = "5545931362:AAFe08KgSSHdcNZaKYavw5xwq4u-5_cybcs";
+        private static TelegramBotClient Bot;
+        static void Main(string[] args)
+        {
+          
+            Bot = new TelegramBotClient(token);
+            using var cts = new CancellationTokenSource();
+            ReceiverOptions receiverOptions = new() { AllowedUpdates = { } };
+            Bot.StartReceiving(Handlers.HandleUpdateAsync,
+                               Handlers.HandleErrorAsync,                           
+                               receiverOptions,
+                               cts.Token); ;
+            Console.WriteLine($"Bot ist gestartet...");
+            Console.ReadLine();
+            cts.Cancel();
+        }
+    }
+}
